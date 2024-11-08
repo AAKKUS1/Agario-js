be68ba66fab5dbee11ccb01b4ecd7e2b6d8fe5ed
AAKKUS1 <aakkus@edenschool.fr>
2024-11-08 15:43:36 +0100

    mousedown circle move

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mycircle = document.getElementById("circle");
+const circle = document.getElementById("circle");
 //let top =0;
 //let left =0;
 
@@ -23,12 +23,48 @@ const mycircle = document.getElementById("circle");
 //})
 
   
-    document.addEventListener("mousemove", (event) => {
-        const posX = event.clientX
-        const posY = event.clientY
+    // document.addEventListener("mousemove", (event) => {
+    //     const posX = event.clientX
+    //     const posY = event.clientY
 
-      mycircle.style.left = `${posX - mycircle.offsetWidth / 2}px`;
-      mycircle.style.top = `${posY - mycircle.offsetHeight / 2}px`;
-    });
+    //   mycircle.style.left = `${posX - mycircle.offsetWidth / 2}px`;
+    //   mycircle.style.top = `${posY - mycircle.offsetHeight / 2}px`;
+    // });
   
   
+
+    let isMoving = false;
+    
+    const startMoving = (event) => {
+
+
+        isMoving = true;
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+    
+
+        circle.style.left = `${mouseX - circle.offsetWidth / 2}px`;
+        circle.style.top = `${mouseY - circle.offsetHeight / 2}px`;
+    
+        document.addEventListener('mousemove', moveCircle);
+    };
+    
+    const stopMoving = () => {
+
+        isMoving = false;
+        document.removeEventListener('mousemove', moveCircle);
+    };
+    
+    const moveCircle = (event) => {
+        if (!isMoving) return;
+        
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+    
+        circle.style.left = `${mouseX - circle.offsetWidth / 2}px`;
+        circle.style.top = `${mouseY - circle.offsetHeight / 2}px`;
+        
+    };
+    
+    document.addEventListener('mousedown', startMoving);
+    document.addEventListener('mouseup', stopMoving);
