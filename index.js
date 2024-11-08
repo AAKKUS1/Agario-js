84c1481b81bdef8384bbce065509e36938011384
AAKKUS1 <aakkus@edenschool.fr>
2024-11-08 14:29:59 +0100

    feat(js)chores

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const mycircle = document.getElementById("circle");
-let top =0;
-let left =0;
+//let top =0;
+//let left =0;
 
-document.addEventListener("keydown",  (event) => {
-   switch (event.key) {
-    case 'ArrowUp':
-        top -= 10; 
-        break;
-        case 'ArrowDown':
-            top += 10; 
-            break;
-            case 'ArrowLeft':
-                left -= 10; 
-                break;
-                case 'ArrowRight':
-                    left += 10; 
-                    break;
-   }
-   mycircle.style.top = top + "px";
-   mycircle.style.left = left + "px";
+//document.addEventListener("keydown",  (event) => {
+   //switch (event.key) {
+    //case 'ArrowUp':
+        //top -= 10; 
+        //break;
+        //case 'ArrowDown':
+            //top += 10; 
+            //break;
+            //case 'ArrowLeft':
+             //   left -= 10; 
+           //     break;
+         //       case 'ArrowRight':
+       //             left += 10; 
+     //               break;
+   //}
+   //mycircle.style.top = top + "px";
+  // mycircle.style.left = left + "px";
 
-})
+//})
+
+  
+    document.addEventListener("mousemove", (event) => {
+        const posX = event.clientX
+        const posY = event.clientY
+
+      mycircle.style.left = `${posX - mycircle.offsetWidth / 2}px`;
+      mycircle.style.top = `${posY - mycircle.offsetHeight / 2}px`;
+    });
+  
+  
